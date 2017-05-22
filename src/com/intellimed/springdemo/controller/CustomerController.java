f4956ac3a1ea8c61bc02b92b7de5a9ebc9baa245
Sam Alsmadi <Sam.Alsmadi@gmail.com>
2017-05-22 15:01:05 -0400

    Add CustomerController

---
+++ src/com/intellimed/springdemo/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package com.intellimed.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@RequestMapping("/list")
+	public String listCustomers(Model model){
+		return "list-customers";
+	}
+}
