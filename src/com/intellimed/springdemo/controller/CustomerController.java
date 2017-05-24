64fd12b8841520480b85b702b0c1cbc1e733d8e9
Sam Alsmadi <Sam.Alsmadi@gmail.com>
2017-05-23 21:13:43 -0400

    Use GetMapping instead of RequestMapping annotations

--- src/com/intellimed/springdemo/controller/CustomerController.java
+++ src/com/intellimed/springdemo/controller/CustomerController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.intellimed.springdemo.dao.CustomerDao;
 import com.intellimed.springdemo.entity.Customer;
@@ -18,7 +20,9 @@ public class CustomerController {
 	@Autowired
 	private CustomerDao customerDao;
 
-	@RequestMapping("/list")
+	//@RequestMapping("/list")
+	//@RequestMapping(path="/list", method=RequestMethod.GET)
+	@GetMapping("/list")
 	public String listCustomers(Model model){
 		List<Customer> customers = customerDao.getCustomers();
 		
